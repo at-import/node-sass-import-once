11abff545066f81029947a04b700bad0fa7e7739
devinchen <deseze74@gmail.com>
2015-09-14 12:04:51 +0800

    Add path match rule

--- index.js
+++ index.js
@@ -122,6 +122,7 @@ var getBowerNames = function getBowerNames(uri) {
     if (this.options.importOnce.css) {
       paths.push(path.resolve(bowerPath, core, 'dist', uri));
       paths.push(path.resolve(bowerPath, core, 'dist', 'css', uri));
+      paths.push(path.resolve(bowerPath, uri));
     }
   }
 
